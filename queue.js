b2be703c3acf657bea6a219a9e98c2843bdd4bf6
emanuelefavero <emafaux89@gmail.com>
2022-12-27 08:42:22 +0100

    add queue.js

---
+++ queue.js
@@ -0,0 +1,44 @@
+// Queue
+class Queue {
+  constructor() {
+    this.queue = []
+  }
+
+  enqueue(item) {
+    this.queue.push(item)
+  }
+
+  // remove the first item
+  dequeue() {
+    return this.queue.shift()
+  }
+
+  getQueue() {
+    return this.queue
+  }
+
+  // return the first item
+  peek() {
+    return this.queue[0]
+  }
+
+  isEmpty() {
+    return this.queue.length === 0
+  }
+
+  size() {
+    return this.queue.length
+  }
+
+  clear() {
+    this.queue = []
+  }
+}
+
+const queue = new Queue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+
+console.log(queue.getQueue()) // [1, 2, 3]
+console.log(queue.dequeue()) // 1
