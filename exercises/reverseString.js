921bbe89836615072127e149f80111618a6ed516
emanuelefavero <emafaux89@gmail.com>
2023-06-29 07:34:33 +0200

    add exercises/reverseString

---
+++ exercises/reverseString.js
@@ -0,0 +1,12 @@
+function reverseString(string) {
+  let reversedString = ''
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i]
+  }
+
+  return reversedString
+}
+
+// -------------------------------
+console.log(reverseString('hello')) // olleh
