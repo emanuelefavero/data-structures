16400eebd15297f18b32c882976db46cf9982ab0
emanuelefavero <emafaux89@gmail.com>
2023-06-29 07:31:17 +0200

    add exercises/stack-reverse-string

---
+++ exercises/stack-reverse-string.js
@@ -0,0 +1,35 @@
+class Stack {
+  constructor() {
+    this.data = []
+  }
+
+  push(item) {
+    this.data.push(item)
+  }
+
+  pop() {
+    return this.data.pop()
+  }
+
+  read() {
+    return this.data[this.data.length - 1]
+  }
+}
+
+function reverseString(string) {
+  const stack = new Stack()
+  let reversedString = ''
+
+  for (let i = 0; i < string.length; i++) {
+    stack.push(string[i])
+  }
+
+  while (stack.read()) {
+    reversedString += stack.pop()
+  }
+
+  return reversedString
+}
+
+// -------------------------------
+console.log(reverseString('hello')) // olleh
