524421feae6cf1f8ee8f0c5eaaa642d48bf71cbf
emanuelefavero <emafaux89@gmail.com>
2023-07-07 13:52:48 +0200

    add exercises/linkedList-print-elements

---
+++ exercises/linkedList-print-elements.js
@@ -0,0 +1,59 @@
+// Add a method to the classic LinkedList (NOT DoublyLinkedList) that prints all the elements of the list
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null
+  }
+
+  insert(value) {
+    let newNode = new Node(value)
+
+    if (!this.head) {
+      this.head = newNode
+      return
+    }
+
+    let current = this.head
+
+    while (current) {
+      if (!current.next) {
+        current.next = newNode
+        return
+      }
+
+      current = current.next
+    }
+  }
+
+  print() {
+    if (!this.head) return null
+
+    let current = this.head
+
+    while (current) {
+      console.log(current.value)
+
+      current = current.next
+    }
+  }
+}
+
+let linkedList = new LinkedList()
+
+linkedList.insert('hello')
+linkedList.insert('how')
+linkedList.insert('are')
+linkedList.insert('you')
+
+linkedList.print()
+// hello
+// how
+// are
+// you
