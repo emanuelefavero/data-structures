b940c83d5f787e83c0d928055bc1c5f34266ce34
emanuelefavero <emafaux89@gmail.com>
2023-07-07 13:59:37 +0200

    add exercises/linkedList-doubly-print-reverse

---
+++ exercises/linkedList-doubly-print-reverse.js
@@ -0,0 +1,64 @@
+// Add a method to the DoublyLinkedList that prints all the elements in reverse order
+
+class Node {
+  constructor(value) {
+    this.previous = null
+    this.value = value
+    this.next = null
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+  }
+
+  insert(value) {
+    let newNode = new Node(value)
+
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+      return
+    }
+
+    let current = this.head
+
+    while (current) {
+      if (!current.next) {
+        current.next = newNode
+        newNode.previous = current
+        this.tail = newNode
+        return
+      }
+
+      current = current.next
+    }
+  }
+
+  reversePrint() {
+    if (!this.head) return null
+
+    let current = this.tail
+
+    while (current) {
+      console.log(current.value)
+
+      current = current.previous
+    }
+  }
+}
+
+let linkedList = new DoublyLinkedList()
+
+linkedList.insert('hello')
+linkedList.insert('how')
+linkedList.insert('are')
+linkedList.insert('you')
+
+linkedList.reversePrint()
+// you
+// are
+// how
+// hello
