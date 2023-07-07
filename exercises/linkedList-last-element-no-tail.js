4cd05036547f8135f5faa8d1dd34103028e00661
emanuelefavero <emafaux89@gmail.com>
2023-07-07 14:05:36 +0200

    add exercises/linkedList-last-element-no-tail

---
+++ exercises/linkedList-last-element-no-tail.js
@@ -0,0 +1,57 @@
+// Add a method to the classic LinkedList (NOT DoublyLinkedList) that returns the last element from the list (without using tail)
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null
+  }
+
+  insert(value) {
+    let newNode = new Node(value)
+
+    if (!this.head) {
+      this.head = newNode
+      return
+    }
+
+    let current = this.head
+
+    while (current) {
+      if (!current.next) {
+        current.next = newNode
+        return
+      }
+
+      current = current.next
+    }
+  }
+
+  getLastElement() {
+    if (!this.head) return null
+
+    let current = this.head
+
+    while (current) {
+      if (!current.next) {
+        return current.value
+      }
+
+      current = current.next
+    }
+  }
+}
+
+let linkedList = new LinkedList()
+
+linkedList.insert('hello')
+linkedList.insert('how')
+linkedList.insert('are')
+linkedList.insert('you')
+
+console.log(linkedList.getLastElement()) // you
