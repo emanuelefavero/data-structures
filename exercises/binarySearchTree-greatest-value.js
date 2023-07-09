2e97088081539c4fa00f0eb9256f1562db0420e5
emanuelefavero <emafaux89@gmail.com>
2023-07-09 05:04:34 +0200

    add exercises/binarySearchTree-greatest-value

---
+++ exercises/binarySearchTree-greatest-value.js
@@ -0,0 +1,78 @@
+// Write an algorithm that finds the greatest value within a binary search tree
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.left = null
+    this.right = null
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null
+  }
+
+  insert(value) {
+    let newNode = new Node(value)
+
+    if (!this.root) {
+      this.root = newNode
+      return
+    }
+
+    let current = this.root
+
+    while (current) {
+      if (value < current.value) {
+        if (!current.left) {
+          current.left = newNode
+          return
+        }
+
+        current = current.left
+      } else {
+        if (!current.right) {
+          current.right = newNode
+          return
+        }
+
+        current = current.right
+      }
+    }
+  }
+
+  // * GET MAX VALUE
+  max() {
+    if (!this.root) return null
+
+    let current = this.root
+
+    while (current) {
+      if (!current.right) {
+        return current.value
+      }
+
+      current = current.right
+    }
+  }
+
+  // * GET MAX VALUE WITH RECURSION
+  max2(node = this.root) {
+    if (!this.root) return null
+
+    if (node.right) return this.max(node.right)
+
+    return node.value
+  }
+}
+
+let BST = new BinarySearchTree()
+
+BST.insert(3)
+BST.insert(5)
+BST.insert(1)
+BST.insert(2)
+
+console.log(BST.max()) // 5
+console.log(BST.max2()) // 5
