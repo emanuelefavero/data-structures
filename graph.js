1a1c93cb86e922a448bf17f0fe1a2a671b1cbede
emanuelefavero <emafaux89@gmail.com>
2023-07-17 09:13:22 +0200

    add breadth first search to graph.js

--- graph.js
+++ graph.js
@@ -1,8 +1,10 @@
 // Graph
 // Object Oriented Graph Implementation (Adjacency List Implementation, Connected Graph, Directed Graph)
-// The time complexity for adding/removing a vertex or an edge (adjacentVertex) is O(1). Searching in a Graph takes O(V + E) time where V is the number of vertices and E is the number of edges. The space complexity for a Graph is O(V + E) where V is the number of vertices and E is the number of edges.
 
 // NOTE: A vertex is a node in a Graph. An edge is a connection between two vertices
+
+// O(1) insert, O(1) remove, O(V + E) search, O(V + E) space
+// V = number of vertices, E = number of edges
 class Graph {
   constructor(value) {
     this.value = value
@@ -61,6 +63,39 @@ class Graph {
     // Return null if the value is not found
     return null
   }
+
+  // * Breadth-First Search Traverse
+  breadthFirstSearch(vertex) {
+    // Create a queue and add the vertex to it
+    let queue = [vertex]
+
+    // Create a hash table to keep track of visited vertices
+    let visitedVertices = {}
+    visitedVertices[vertex.value] = true
+
+    // Loop as long as the queue is not empty
+    while (queue.length) {
+      // Remove the first vertex from the queue
+      let removedVertex = queue.shift()
+
+      // Print the vertex
+      console.log(removedVertex.value)
+
+      // Loop through the adjacent vertices of the removed vertex
+      for (let adjacentVertex of removedVertex.adjacentVertices) {
+        // If the adjacent vertex has not been visited
+        if (!visitedVertices[adjacentVertex.value]) {
+          // Mark the adjacent vertex as visited
+          visitedVertices[adjacentVertex.value] = true
+
+          // Add the adjacent vertex to the queue
+          queue.push(adjacentVertex)
+        }
+      }
+    }
+  }
+
+  // TIP: DFS vs BFS: If you want to find the shortest path between two vertices, use BFS. If you want to traverse all the vertices in a graph, use DFS. DFS is useful when searching for a grandchild or a great-grandchild in a family tree. BFS is useful when searching for the closest person or a friend in a social network (e.g. LinkedIn, Facebook)
 }
 
 let alice = new Graph('Alice')
@@ -84,3 +119,9 @@ alice.depthFirstSearch(alice)
 console.log(alice.depthFirstSearchVertex(alice, 'Bob'))
 // Graph { value: 'Bob', adjacentVertices: [ Graph { value: 'Derek', adjacentVertices: [] } ] }
 console.log(derek.depthFirstSearchVertex(alice, 'Mark')) // null
+
+alice.breadthFirstSearch(alice)
+// Alice
+// Bob
+// Candy
+// Derek
