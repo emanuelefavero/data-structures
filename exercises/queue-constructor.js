3fdab47f9116fb5e7243217bb5a57411925870f2
emanuelefavero <emafaux89@gmail.com>
2023-08-03 08:53:47 +0200

    add exercises/stack-constructor

---
+++ exercises/queue-constructor.js
@@ -0,0 +1,20 @@
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class Queue {
+  constructor(value) {
+    let newNode = new Node(value)
+
+    this.first = newNode
+    this.last = newNode
+    this.length = 1
+  }
+}
+
+let queue = new Queue(1)
+
+console.log(queue)
