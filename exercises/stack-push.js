8a2aac287a776b6e84f0c6819bfbbe839d582efc
emanuelefavero <emafaux89@gmail.com>
2023-08-03 08:43:39 +0200

    add exercises/stack-push

--- exercises/stack-push.js
+++ exercises/stack-push.js
@@ -6,14 +6,29 @@ class Node {
 }
 
 class Stack {
-  // * CONSTRUCTOR
   constructor(value) {
     let newNode = new Node(value)
     this.top = newNode
     this.length = 1
   }
+
+  // * PUSH
+  push(value) {
+    let newNode = new Node(value)
+
+    if (this.length === 0) {
+      this.top = newNode
+    } else {
+      newNode.next = this.top
+      this.top = newNode
+    }
+
+    this.length++
+    return this
+  }
 }
 
 let stack = new Stack(1)
+stack.push(2)
 
 console.log(stack)
