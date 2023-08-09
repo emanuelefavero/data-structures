38b51b683218efa5e8b7fd74de27a90bd1002976
emanuelefavero <emafaux89@gmail.com>
2023-08-09 07:28:02 +0200

    add exercises/hashTable-constructor

---
+++ exercises/hashTable-constructor.js
@@ -0,0 +1,19 @@
+class HashTable {
+  constructor(size = 7) {
+    this.dataMap = new Array(size)
+  }
+
+  _hash(key) {
+    let hash = 0
+
+    for (let i = 0; i < key.length; i++) {
+      // TIP: Prime numbers (like 23) in the hash are helpful in reducing collisions. We also use the modulo operator to reduce the hash value to a smaller number.
+      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length
+    }
+
+    return hash
+  }
+}
+
+let hashTable = new HashTable()
+console.log(hashTable) // HashTable { dataMap: [ <7 empty items> ] }
