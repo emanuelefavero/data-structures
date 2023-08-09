e26dc10e8d4ae7316b3557d8452cf60ace0bca2b
emanuelefavero <emafaux89@gmail.com>
2023-08-09 07:59:57 +0200

    add exercises/hashTable-keys

---
+++ exercises/hashTable-keys.js
@@ -0,0 +1,66 @@
+class HashTable {
+  constructor(size = 7) {
+    this.dataMap = new Array(size)
+  }
+
+  _hash(key) {
+    let hash = 0
+
+    for (let i = 0; i < key.length; i++) {
+      // TIP: Prime numbers (like 23) in the hash are helpful in reducing collisions. We also use the modulo operator to reduce the hash value to a smaller number.
+      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length
+    }
+
+    return hash
+  }
+
+  set(key, value) {
+    let index = this._hash(key)
+
+    // If there's no data at the index, set the index to an empty array
+    if (!this.dataMap[index]) {
+      this.dataMap[index] = []
+    }
+
+    // Push the key/value pair into the index
+    this.dataMap[index].push([key, value])
+    return this
+  }
+
+  get(key) {
+    let index = this._hash(key)
+
+    if (this.dataMap[index]) {
+      for (let i = 0; i < this.dataMap[index].length; i++) {
+        // If the key matches, return the value. e.g. [ [ 'black shirt', 18 ] ]
+        if (this.dataMap[index][i][0] === key) {
+          return this.dataMap[index][i][1]
+        }
+      }
+    }
+
+    return undefined // If the key doesn't exist, return undefined
+  }
+
+  // * GET KEYS AS AN ARRAY
+  keys() {
+    let allKeys = []
+
+    for (let i = 0; i < this.dataMap.length; i++) {
+      if (this.dataMap[i]) {
+        for (let j = 0; j < this.dataMap[i].length; j++) {
+          allKeys.push(this.dataMap[i][j][0])
+        }
+      }
+    }
+
+    return allKeys
+  }
+}
+
+let hashTable = new HashTable()
+
+hashTable.set('black shirt', 18)
+hashTable.set('red shirt', 8)
+
+console.log(hashTable.keys()) // [ 'red shirt', 'black shirt' ]
