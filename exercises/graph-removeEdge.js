3764ab765bf0398f20f3c0c883c60b4de51197a0
emanuelefavero <emafaux89@gmail.com>
2023-08-11 04:17:11 +0200

    add exercises/graph-removeEdge

---
+++ exercises/graph-removeEdge.js
@@ -0,0 +1,53 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {}
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = []
+      return true
+    }
+
+    return false
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2)
+      this.adjacencyList[vertex2].push(vertex1)
+      return true
+    }
+
+    return false
+  }
+
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (item) => item !== vertex2
+      )
+
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (item) => item !== vertex1
+      )
+      return true
+    }
+
+    return false
+  }
+}
+
+let graph = new Graph()
+
+graph.addVertex('A')
+graph.addVertex('B')
+graph.addVertex('C')
+
+graph.addEdge('A', 'B')
+graph.addEdge('B', 'C')
+
+graph.removeEdge('A', 'B')
+
+console.log(graph.adjacencyList)
+// { A: [], B: [ 'C' ], C: [ 'B' ] }
