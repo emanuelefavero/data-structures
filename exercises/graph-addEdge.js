7cbb9d7a19a84e4c1f81afbfc17c6c5198ae2314
emanuelefavero <emafaux89@gmail.com>
2023-08-11 04:09:23 +0200

    add exercises/graph-addEdge

---
+++ exercises/graph-addEdge.js
@@ -0,0 +1,36 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {}
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = []
+      return true
+    }
+
+    return false
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2)
+      this.adjacencyList[vertex2].push(vertex1)
+      return true
+    }
+
+    return false
+  }
+}
+
+let graph = new Graph()
+
+graph.addVertex('A')
+graph.addVertex('B')
+graph.addVertex('C')
+
+graph.addEdge('A', 'B')
+graph.addEdge('B', 'C')
+
+console.log(graph.adjacencyList)
+// { A: [ 'B' ], B: [ 'A', 'C' ], C: [ 'B' ] }
