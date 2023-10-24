6fcc073fedd5da12418ce630002278a96704a6a8
emanuelefavero <emafaux89@gmail.com>
2023-10-24 09:35:32 +0200

    add findShortestPath to binaryTree-breadthFirst...

--- binaryTree-breadthFirstTraversal.js
+++ binaryTree-breadthFirstTraversal.js
@@ -73,6 +73,27 @@ class BinaryTree {
 
     return result
   }
+
+  // * Breadth First Search - find shortest path
+  // return the shortest path to a node
+  findShortestPath(value) {
+    if (!this.root) return
+
+    let queue = [this.root]
+    let count = 0
+
+    while (queue.length) {
+      let current = queue.shift()
+
+      if (current.value === value) return count
+      if (current.left) queue.push(current.left)
+      if (current.right) queue.push(current.right)
+
+      count++
+    }
+
+    return -1
+  }
 }
 
 let tree = new BinaryTree()
@@ -83,3 +104,21 @@ tree.insert(3)
 
 tree.BFS() // 10, 6, 15, 3
 console.log(tree.levelOrder()) // [ 10, 6, 15, 3 ]
+
+tree.insert(8)
+tree.insert(20)
+tree.insert(2)
+tree.insert(25)
+
+/*
+TREE STRUCTURE
+          10
+        /    \
+      6       15
+    /  \     /  \
+  3     8  20    25
+ /
+2
+*/
+
+console.log(tree.findShortestPath(25)) // 7
