26476b41c6bbbfedb553e1018505cc9e45741722
emanuelefavero <emafaux89@gmail.com>
2024-01-31 03:40:26 +0100

    refactor trie-autocomplete

--- trie-autocomplete.js
+++ trie-autocomplete.js
@@ -1,55 +1,46 @@
 // Trie - autocomplete
 // O(k) search, insert, delete
 
-class TrieNode {
+class Node {
   constructor() {
-    // NOTE: Each node in the trie is an object (hash table) with keys that are the characters in the word
     this.children = {}
   }
 }
 
 class Trie {
   constructor() {
-    this.root = new TrieNode()
+    this.root = new Node()
   }
 
   // * INSERT
   insert(word) {
-    let currentNode = this.root
+    let current = this.root
 
-    for (let i = 0; i < word.length; i++) {
-      let char = word[i]
-
-      if (!currentNode.children[char]) {
-        let newNode = new TrieNode()
-        currentNode.children[char] = newNode
-
-        currentNode = newNode
-      } else {
-        currentNode = currentNode.children[char]
+    for (let char of word) {
+      if (!current.children[char]) {
+        current.children[char] = new Node()
       }
+
+      current = current.children[char]
     }
 
-    currentNode.children['*'] = true
+    current.children['*'] = true
   }
 
   // * SEARCH
   search(word) {
-    let currentNode = this.root
+    let current = this.root
 
-    for (let i = 0; i < word.length; i++) {
-      let char = word[i]
-
-      if (!currentNode.children[char]) return false
-
-      currentNode = currentNode.children[char]
+    for (let char of word) {
+      if (!current.children[char]) return false
+      current = current.children[char]
     }
 
-    // We pass the currentNode here so that we can use this search method in the autoComplete method
-    return currentNode
+    // We pass the current here so that we can use this search method in the autoComplete method
+    return current
 
     // NOTE: If we don't need to use this search method in the autoComplete method, we can just return this:
-    // return currentNode.children['*'] === true
+    // return current.children['*'] === true
   }
 
   // * COLLECT ALL WORDS
@@ -58,11 +49,8 @@ class Trie {
     for (let key in node.children) {
       let child = node.children[key]
 
-      if (key === '*') {
-        words.push(word)
-      } else {
-        this.collectAllWords(child, word + key, words)
-      }
+      if (key === '*') words.push(word)
+      else this.collectAllWords(child, word + key, words)
     }
 
     return words
@@ -71,13 +59,16 @@ class Trie {
   // * AUTOCOMPLETE
   // Call search() and collectAllWords() recursively to get all the words that start with the prefix
   autoComplete(prefix) {
-    let currentNode = this.search(prefix)
+    let current = this.search(prefix)
 
-    if (!currentNode) return null
+    if (!current) return null
 
-    return this.collectAllWords(currentNode, prefix) // we also need to pass the prefix so that we can add it to the words we collect
+    return this.collectAllWords(current, prefix) // we also need to pass the prefix so that we can add it to the words we collect
   }
 }
+
+// -----------------------------------
+// TESTS
 
 let trie = new Trie()
 
